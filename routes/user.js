9e75be2f049cc017ff807d3bebd0e9831e72ab26
JwCho1 <choikijoo13@naver.com>
2023-08-14 15:25:32 +0900

    asd

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const conn = require("../config/database");
-let url = 'https://connectschool.servehttp.com'
+let url = 'https://connectschool.servehttp.com/'
 
 
 router.post("/write", (req, res) => {
