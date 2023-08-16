108ef671294a5a6304f12e0672264600c1d32dfc
JwCho1 <choikijoo13@naver.com>
2023-08-16 11:35:22 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const conn = require("../config/database");
-let url = 'http://localhost:3000'
+let url = 'https://connectschool.servehttp.com/'
 
 router.get("/", (req, res) => {
   res.render('screen/login')
